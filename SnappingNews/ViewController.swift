23b910d58ff239742bff22cc3709e97f39fd6b93
Jackie Zhang <viperking@163.com>
2016-10-15 15:35:20 +0800

    Initial Commit

---
+++ SnappingNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnappingNews
+//
+//  Created by Jackie Zhang on 2016/10/15.
+//  Copyright © 2016年 Jackie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
