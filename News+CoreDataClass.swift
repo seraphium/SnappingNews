ee856263ef2230ce1d321c2ab478365a64abe680
Jackie Zhang <viperking@163.com>
2016-10-15 16:32:07 +0800

    first commit on Snapping news using Carthage to manage frameworks

---
+++ News+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  News+CoreDataClass.swift
+//  SnappingNews
+//
+//  Created by Jackie Zhang on 2016/10/15.
+//  Copyright © 2016年 Jackie Zhang. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class News: NSManagedObject {
+
+}
--- SnappingNews/ViewController.swift
+++ SnappingNews/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import HandyJSON
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.saveData()
+
+        self.fetchData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func saveData() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let news = News(context: context)
+        
+        news.title = "test"
+        news.author_name = "zz"
+        news.uniquekey = "avdsv43"
+        
+        do {
+            try context.save()
+        } catch let error {
+            print ("context saving failed:\(error)")
+        }
+    }
+
+    func fetchData() {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest : NSFetchRequest<News> = News.fetchRequest()
+        
+        do {
+            let newsList = try context.fetch(fetchRequest)
+            
+            for new in newsList {
+                print (new.title)
+            }
+        } catch let error {
+            print("fetch data failed \(error)")
+        }
+    }
 
 }
 
